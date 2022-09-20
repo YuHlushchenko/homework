29a3332cfbd3e53d5748cac79aa20cea2d2619df
YuHlushchenko <yuhlushchenko@gmail.com>
2022-09-20 03:38:45 +0300

    OurServices

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import MainSlider from '../components/MainSlider/MainSlider'
+import OurServices from '../components/OurServices/OurServices'
 import styles from '../styles/home.module.sass'
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
-        <section className={styles.mainSliderSection}>
-            <MainSlider />
-        </section>
+      <section className={styles.mainSliderSection}>
+        <MainSlider />
+      </section>
+
+      <section className={styles.ourServicesSection}>
+        <OurServices />
+      </section>
     </div>
   )
 }
