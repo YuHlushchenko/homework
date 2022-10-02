5ec65f56bff18a2befff1e3677cfe7c3bf20084c
YuHlushchenko <yuhlushchenko@gmail.com>
2022-10-02 23:59:07 +0300

    Partners page

--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import styles from '../styles/partners.module.sass'
+import Footer from '../components/Footer/Footer'
 
 const Partners = () => {
+  const partners = [
+    {
+      imgSrc: '/partnersLogos/antei.png',
+      alt: 'antei'
+    },
+    {
+      imgSrc: '/partnersLogos/knauf.png',
+      alt: 'knauf'
+    },
+    {
+      imgSrc: '/partnersLogos/artDoor.png',
+      alt: 'artDoor'
+    },
+    {
+      imgSrc: '/partnersLogos/grohe.png',
+      alt: 'grohe'
+    },
+  ]
+
   return (
-    <div>Partners</div>
+    <>
+      <section className={styles.partnersSectionContainer}>
+        <div className={styles.textContaner}>
+          <h2>партнери</h2>
+          <p>Наша компанія завжди відкрита для взаємовигідного співробітництва. Ми готові розглянути будь-які пропозиції від постачальників сировини і комплектуючих, дизайнерів, суміжників, торгових мереж і споживачів нашої продукції. При укладанні договору про довгострокову співпрацю ми пропонуємо роботу з нами на особливих умовах, орієнтуючись як на побажання потенційного партнера, так і на пріоритети власної компанії. Угода про партнерство відкриває шлях до зручної системі взаєморозрахунків, ідеально оптимізованої логістиці, спільним рекламним кампаніям і більш ефективному просуванню продукції підприємств-партнерів на український ринок. </p>
+        </div>
+
+        <div className={styles.patnersLogosContainer}>
+          {
+            partners.map((item, index) => {
+              return (
+                <div className={styles.logoContainer} key={index}>
+                  <img src={item.imgSrc} alt={item.alt} width='100%' />
+                </div>
+              )
+            })
+          }
+        </div>
+      </section>
+
+      <section>
+        <Footer />
+      </section>
+    </>
   )
 }
 
