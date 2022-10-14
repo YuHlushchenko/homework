a4b3e7dc5754e0b279cc458995e3b1c241758f31
yuliia159357 <49586387+yuliia159357@users.noreply.github.com>
2022-10-14 23:33:31 +0300

    fixed

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -4,28 +4,31 @@ import styles from './OurServices.module.sass'
 const OurServices = () => {
   return (
     <div className={styles.ourServicesContainer}>
-        <h2>наші послуги</h2>
-        <p>Ми проводимо ремонтно-будівничі роботи в квартирах будь-якого типу, <br /> площі і технічного стану</p>
-        
-        <div className={styles.servicesContainer}>
-            <div className={`${styles.serviceBlock} ${styles.img1}`}>
-                <div className={styles.bottomContainer}>
-                    <span>Ремонт квартир під ключ</span>
-                </div>
-            </div>
+      <h2>наші послуги</h2>
+      <p>
+        Ми проводимо ремонтно-будівничі роботи в квартирах будь-якого типу,{' '}
+        <br /> площі і технічного стану
+      </p>
 
-            <div className={`${styles.serviceBlock} ${styles.img2}`}>
-                <div className={styles.bottomContainer}>
-                    <span>Дизайн інтер’єру</span>
-                </div>
-            </div>
-
-            <div className={`${styles.serviceBlock} ${styles.img3}`}>
-                <div className={styles.bottomContainer}>
-                    <span>Будівництво будинків</span>
-                </div>
-            </div>
+      <div className={styles.servicesContainer}>
+        <div className={`${styles.serviceBlock} ${styles.img1}`}>
+          <div className={styles.bottomContainer}>
+            <span>Ремонт квартир під ключ</span>
+          </div>
         </div>
+
+        <div className={`${styles.serviceBlock} ${styles.img2}`}>
+          <div className={styles.bottomContainer}>
+            <span>Дизайн інтер’єру</span>
+          </div>
+        </div>
+
+        <div className={`${styles.serviceBlock} ${styles.img3}`}>
+          <div className={styles.bottomContainer}>
+            <span>Будівництво будинків</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/RepairTypesContainer/RepairTypesContainer.jsx
+++ src/components/RepairTypesContainer/RepairTypesContainer.jsx
@@ -3,61 +3,59 @@ import styles from './RepairTypesContainer.module.sass'
 import RepairTypesItem from '../RepairTypesItem/RepairTypesItem'
 
 const dataRepair = {
-    economy: {
-        imgSrc: '/servicesPageImgs/moreDetails/img1.png',
-        title: 'Економ варіант',
-        price: '300 грн м',
-        time: '45-50 днів',
-        bttnLink: '/services/economy',
-        isReverse: false,
-        // tableName: 'economyTable',
-    },
+  economy: {
+    imgSrc: '/servicesPageImgs/moreDetails/img1.png',
+    title: 'Економ варіант',
+    price: '300 грн м',
+    time: '45-50 днів',
+    bttnLink: '/services/economy',
+    isReverse: false,
+    // tableName: 'economyTable',
+  },
 
-    eurorepair: {
-        imgSrc: '/servicesPageImgs/moreDetails/img2.png',
-        title: 'Економ варіант',
-        price: '300 грн м',
-        time: '45-50 днів',
-        bttnLink: '/services/eurorepair',
-        isReverse: true,
-        // tableName: 'economyTable',
-    },
+  eurorepair: {
+    imgSrc: '/servicesPageImgs/moreDetails/img2.png',
+    title: 'Економ варіант',
+    price: '300 грн м',
+    time: '45-50 днів',
+    bttnLink: '/services/eurorepair',
+    isReverse: true,
+    // tableName: 'economyTable',
+  },
 
-    business: {
-        imgSrc: '/servicesPageImgs/moreDetails/img3.png',
-        title: 'бізнес варіант',
-        price: '800 грн м',
-        time: '45-50 днів',
-        bttnLink: '/services/business',
-        isReverse: false,
-        // tableName: 'economyTable',
-    },
+  business: {
+    imgSrc: '/servicesPageImgs/moreDetails/img3.png',
+    title: 'бізнес варіант',
+    price: '800 грн м',
+    time: '45-50 днів',
+    bttnLink: '/services/business',
+    isReverse: false,
+    // tableName: 'economyTable',
+  },
 }
 
 const dataKeys = Object.keys(dataRepair)
 
 const RepairTypesContainer = () => {
-    return (
-        <div className={styles.repairTypesContainer}>
-            <h2>Види ремонту які ми виконуємо</h2>
-            <p>Ми проводимо ремонтно-будівничі роботи в квартирах будь-якого типу, площі і технічного стану</p>
-            <div className={styles.mainContentContainer}>
-                {
-                    dataKeys.map((item, index) => {
-                        console.log(dataRepair[item])
-                        return <div
-                            key={index}
-                            className={styles.repairTypesItemContainer}
-                        >
-                            <RepairTypesItem
-                                itemData={dataRepair[item]}
-                            />
-                        </div>
-                    })
-                }
+  return (
+    <div className={styles.repairTypesContainer}>
+      <h2>Види ремонту які ми виконуємо</h2>
+      <p>
+        Ми проводимо ремонтно-будівничі роботи в квартирах будь-якого типу,
+        площі і технічного стану
+      </p>
+      <div className={styles.mainContentContainer}>
+        {dataKeys.map((item, index) => {
+          console.log(dataRepair[item])
+          return (
+            <div key={index} className={styles.repairTypesItemContainer}>
+              <RepairTypesItem itemData={dataRepair[item]} />
             </div>
-        </div>
-    )
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default RepairTypesContainer
