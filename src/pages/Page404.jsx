87197fd01aa3c034ee3988a1e54486e34f70c573
YuHlushchenko <yuhlushchenko@gmail.com>
2022-10-30 02:34:20 +0300

    Page 404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -5,7 +5,11 @@ import styles from '../styles/page404.module.sass'
 const Page404 = () => {
   return (
     <PageWrapper>
-      <div className={styles.container}>Page 404</div>
+      <div className={styles.container}>
+        <h2>404</h2>
+        <p>Сторінка, яку Ви шукаєте не знайдена або не існує.</p>
+        <p>Повернутися <a href='/'>На головну</a>.</p>
+      </div>
     </PageWrapper>
   )
 }
