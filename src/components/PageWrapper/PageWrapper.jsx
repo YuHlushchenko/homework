f88adda4ad64dc734d1f8e915086a62d22ed03d4
YuHlushchenko <yuhlushchenko@gmail.com>
2022-10-30 02:05:40 +0300

    To Top Button

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Navigation from '@components/Navigation/Navigation'
 import Footer from '@components/Footer/Footer'
+import ToTopButton from '@components/UI/ToTopButton/ToTopButton'
 
 const PageWrapper = ({ children }) => {
-  // console.log(children)
   return (
     <>
       <Navigation />
       {children}
+      <ToTopButton />
       <Footer />
     </>
   )
