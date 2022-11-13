66609548296be59cbe9ab215abe367e41d926a12
YuHlushchenko <yuhlushchenko@gmail.com>
2022-11-13 20:12:46 +0200

    Footer and Home have been finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
 import Achievements from '@components/Achievements/Achievements'
 import MainSlider from '@components/MainSlider/MainSlider'
 import OurServices from '@components/OurServices/OurServices'
 import CustomMap from '@components/CustomMap/CustomMap'
-import styles from '../styles/home.module.sass'
 import AnyQuestions from '@components/AnyQuestions/AnyQuestions'
 import PageWrapper from '@components/PageWrapper/PageWrapper'
 import RepairTypesContainer from '@components/RepairTypesContainer/RepairTypesContainer'
 import ChooseUs from '@components/ChooseUs/ChooseUs'
 import CalculatorItem from '@components/CalculatorItem/CalculatorItem'
+import styles from '../styles/home.module.sass'
 
 const Home = () => {
   return (
     <PageWrapper>
       <div className={styles.homeContainer}>
-        <section className={styles.mainSliderSection}>
-          <MainSlider />
-        </section>
-
-        <section className={styles.repairTypesContainer}>
-          <RepairTypesContainer />
-        </section>
+        <MainSlider />
+        <RepairTypesContainer />
 
         <section className={styles.calculatorContainer}>
-          <CalculatorItem title='Розрахуйте вартість ремонту'/>
+          <CalculatorItem
+            title='Розрахуйте вартість ремонту'
+          />
         </section>
 
         <section className={styles.ourServicesSection}>
@@ -31,17 +27,9 @@ const Home = () => {
         </section>
 
         <ChooseUs />
-
-        <section className={styles.achievementsSection}>
-          <Achievements />
-        </section>
-
-        <section>
-          <CustomMap />
-        </section>
-
+        <Achievements />
+        <CustomMap />
         <AnyQuestions />
-
       </div>
     </PageWrapper>
   )
